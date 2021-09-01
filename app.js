f3add0a8709e4059166383fa4538a69775992670
Gregory Marklew <greg_marklew@hotmail..com>
2021-09-01 09:28:27 +0100

    solution

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 const express = require('express');
 
 const hbs = require('hbs');
@@ -13,6 +14,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // ...
 
@@ -20,6 +22,29 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+//Promises
+//Promises are JavaScript objects that might jave or not a value in the near future
+//Traditional way to consume promises
+// app.get('/beers', (req, res) => {
+//   punkAPI.getBeers().then(beers => {
+//     console.log(beers);
+//     res.render('beers', { beers });
+//   });
+// });
+
+// Modern way to consume promises
+app.get('/beers', async (req, res) => {
+  const beers = await punkAPI.getBeers();
+  console.log(beers);
+  res.render('beers', { beers });
+});
+
+app.get('/random-beers', (req, res) => {
+  punkAPI.getRandom().then(response => {
+    console.log(response);
+    res.render('random-beers', { response });
+  });
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
